b54ec88e548a3797bf45ff6c594551a0140ed626
Igor Lesnevskiy <lesnevskii.igor@gmail.com>
2020-04-26 18:59:32 +0300

    [i] add gh-pages deploy

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,7 +1,25 @@
-const FtpDeploy = require('ftp-deploy');
-const ftpDeploy = new FtpDeploy();
-const config = require('../deploy.config.js');
+const FtpDeploy = require("ftp-deploy");
+const ghpages = require("gh-pages");
 
-ftpDeploy.deploy(config)
-	.then(res => console.log('finished:', res))
-	.catch(err => console.log(err))
+const ftpDeploy = new FtpDeploy();
+const config = require("../deploy.config.js");
+
+ftpDeploy
+  .deploy(config.ftp)
+  .then((res) => console.log("finished upload with ftp"))
+  .then((res) => {
+    ghpages.publish(
+      config.github.source,
+      {
+        ...config.github,
+      },
+      function (err) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("finished upload with gh-page");
+        }
+      }
+    );
+  })
+  .catch((err) => console.log(err));
