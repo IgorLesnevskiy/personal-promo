11514074f4d4703df6545980bb44a51ce68608d6
retroboy <lesnevskii.igor@gmail.com>
2025-10-24 12:07:32 +0200

    updated info; updated yarn version; fixed eslint problems

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,24 @@
-import "modernizr";
-import * as Wow from "wow.js";
+import 'modernizr';
+import * as Wow from 'wow.js';
 
-import "../styles/app.scss";
+import '../styles/app.scss';
 
 const AppModule = function () {
-    return {
-        init() {
-            initAnimations();
-            initConsoleEasterEgg();
-        },
-    };
+	return {
+		init() {
+			initAnimations();
+			initConsoleEasterEgg();
+		},
+	};
 
-    function initAnimations() {
-        new Wow().init();
-    }
+	function initAnimations() {
+		new Wow().init();
+	}
 
-    function initConsoleEasterEgg() {
-        /* eslint-disable-next-line */
+	function initConsoleEasterEgg() {
+		/* eslint-disable-next-line */
         console.log(
-            `%c
+			`%c
             _________________________________
             < Hello! Looking for something? >
             ---------------------------------
@@ -27,13 +27,13 @@ const AppModule = function () {
                         (__)\\       )\\/\\
                             ||----w |
                             ||     ||`,
-            "font-family:monospace; font-size: 16px; color: #0079c1;"
-        );
-    }
+			'font-family:monospace; font-size: 16px; color: #0079c1;'
+		);
+	}
 };
 
 const app = new AppModule();
 
-window.addEventListener("load", () => {
-    app.init();
+window.addEventListener('load', () => {
+	app.init();
 });
